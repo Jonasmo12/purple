65a781f4430b64f55c6ecd89636817d8f7cd921a
Jonas Moloto <jonasmoloto@outlook.com>
2023-04-21 07:05:03 +0200

    testing changes

--- src/main/java/com/discerned/purple/PurpleApplication.java
+++ src/main/java/com/discerned/purple/PurpleApplication.java
@@ -16,7 +16,7 @@ public class PurpleApplication {
 
 	@RequestMapping("")
 	public String hello() {
-		return "hello from azure for discerned";
+		return "hello from azure vm for discerned";
 	}
 
 }
