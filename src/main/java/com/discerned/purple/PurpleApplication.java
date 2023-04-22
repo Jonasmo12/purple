81b42a27a9d11e078229e1226b13cdc2f469dfe3
Jonas Moloto <jonasmoloto@outlook.com>
2023-04-22 22:19:30 +0200

    successfull set of tomcat server in vm ubuntu 18.04

--- src/main/java/com/discerned/purple/PurpleApplication.java
+++ src/main/java/com/discerned/purple/PurpleApplication.java
@@ -2,11 +2,17 @@ package com.discerned.purple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PurpleApplication { 
+public class PurpleApplication extends SpringBootServletInitializer { 
 
-	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+		return springApplicationBuilder.sources(PurpleApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PurpleApplication.class, args);
 	}
