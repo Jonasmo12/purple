1342f5f050b83e1ad15a649f05ce4e8d2f25c979
Jonas Moloto <jonasmoloto@outlook.com>
2023-05-21 17:24:18 +0200

    add search feature

--- src/main/java/com/discerned/purple/PurpleApplication.java
+++ src/main/java/com/discerned/purple/PurpleApplication.java
@@ -7,23 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class PurpleApplication{
-
-//<<<<<<< HEAD
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
-//		return springApplicationBuilder.sources(PurpleApplication.class);
-//	}
-//
-//=======
-////	@Override
-////    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
-////		return springApplicationBuilder.sources(PurpleApplication.class);
-////	}
-//
-//>>>>>>> testing
 	public static void main(String[] args) {
 		SpringApplication.run(PurpleApplication.class, args);
 	}
-
-	
 }
--- src/main/java/com/discerned/purple/patient/PatientController.java
+++ src/main/java/com/discerned/purple/patient/PatientController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.access.prepost.PreAuthorize;
 //import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -39,6 +40,11 @@ public class PatientController {
     @PostMapping("/registration")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody Patient patient) {
         return ResponseEntity.ok(patientService.signUpPatient(patient));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+        return ResponseEntity.ok("logout successful");
     }
 
     @PostMapping("/refresh-token")
